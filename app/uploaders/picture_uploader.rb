81fc653ed538b917364fb7ecddbd8a7c6884886f
Edouga jean <edougajean@gmail.com>
2017-06-15 00:23:54 +0100

    Initial commit

---
+++ app/uploaders/picture_uploader.rb
@@ -0,0 +1,65 @@
+class PictureUploader < CarrierWave::Uploader::Base
+
+
+  # Include RMagick or MiniMagick support: (comme on redimensionne l'image on choisit l'une des 2 gem)
+  # include CarrierWave::RMagick
+   include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+   #pour deployer avec nos images sur heruko en suite on ira dans config/initializer on
+   #créé un fichier carrier_wave.rb
+   if Rails.env.production?
+     storage :fog
+   else
+     storage :file
+   end
+
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{model.id}"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  # Process files as they are uploaded: (on redimensionne)
+   process scale: [200, 300]
+  #
+   def scale(width, height)
+     resize_to_fill width, height
+   end
+
+  # Create different versions of your uploaded files:
+   version :thumb do
+     process resize_to_fit: [50, 50]
+   end
+
+   version :large do
+     process resize_to_fit: [100, 100]
+   end
+
+    version :medium do
+     process resize_to_fit: [150, 150]
+   end
+
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this: (le type de format)
+   def extension_whitelist
+     %w(jpg jpeg  png)
+   end
+
+  # Override the filename of the uploaded files:
+  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+   def filename
+    "#{mounted_as}.jpg" if original_filename
+   end
+end
---
+++ config/routes.rb
@@ -0,0 +1,41 @@
+Rails.application.routes.draw do
+
+
+  root to: 'pages#home'
+
+  #tous le code commenté est idem que la ligne 23
+
+  #get '/recipes', to: 'recipes#index'
+
+  #get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+
+  #post '/recipes', to: 'recipes#create'
+
+  #get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+
+  #patch '/recipes/:id', to: 'recipes#update'
+
+  #get  '/recipes/:id', to: 'recipes#show', as: 'recipe'
+
+ # delete '/recipes/:id', to: 'recipes#destroy'
+
+
+  resources :recipes do
+    member do
+      post 'like'
+    end
+  end
+
+  resources :chefs, except: [:new, :destroy]
+
+  get '/register', to: 'chefs#new'
+
+  get '/login', to: "logins#new"
+  post '/login', to: "logins#create"
+  get '/logout', to: "logins#destroy"
+
+  resources :styles, only: [:new, :create, :show]
+
+  resources :ingredients, only: [:new, :create, :show]
+
+end
